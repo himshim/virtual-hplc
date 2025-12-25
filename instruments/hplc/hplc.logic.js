9c179b5ff96ed2e1e251247137d732f360e7aef0
himshim26 <97538806+himshim@users.noreply.github.com>
2025-12-25 19:03:18 +0530

    Update hplc.logic.js

--- instruments/hplc/hplc.logic.js
+++ instruments/hplc/hplc.logic.js
@@ -1,15 +1,35 @@
 import { renderGraph } from "../../graph/graphEngine.js";
 import { generateHPLCData } from "./hplc.dataGenerator.js";
 
+/* Column chemistry presets */
+const columnPresets = {
+  C18: { factor: 1.0, resistance: 1.2 },
+  C8: { factor: 0.7, resistance: 1.0 },
+  Silica: { factor: 1.4, resistance: 1.5 }
+};
+
+export function updateColumn(state) {
+  const preset = columnPresets[state.column.type];
+
+  state.column.factor = preset.factor *
+    (state.column.length / 150);
+
+  state.column.resistance =
+    preset.resistance *
+    (state.column.length / 150) *
+    (5 / state.column.particleSize);
+
+  state.column.efficiency =
+    (state.column.length / state.column.particleSize) / 30;
+}
+
 export function updateMobilePhase(state) {
   const A = state.mobilePhase.solventA;
   const B = state.mobilePhase.solventB;
 
-  // Elution strength (RP-HPLC approximation)
   state.mobilePhase.strength =
     0.3 + (B.percent / 100) * 0.7;
 
-  // Viscosity (weighted average)
   state.mobilePhase.viscosity =
     (A.percent * A.viscosity +
      B.percent * B.viscosity) / 100;
