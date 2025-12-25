88f8be2c821ac5c8191ebeba907f082805f89733
himshim26 <97538806+himshim@users.noreply.github.com>
2025-12-25 20:04:05 +0530

    Delete hplc.logic.js

--- instruments/hplc/hplc.logic.js
+++
@@ -1,9 +0,0 @@
-export function calculateEstimatedRT(state) {
-  const comp = state.sample.components[0];
-
-  return (
-    2 +
-    (comp.hydrophobicity * state.column.factor) /
-    (state.mobilePhase.strength * state.flow)
-  );
-}
